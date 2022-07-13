28522bac06bd69492f5410d8ca5d54342588d478
Fox <kaitiefox@gmail.com>
2022-07-13 10:06:27 -0400

    update

--- karate-chop/src/BinaryChop1.php
+++ karate-chop/src/BinaryChop1.php
@@ -8,4 +8,15 @@ class BinaryChop1
     {
         return "yay!";
     }
+
+    public function halveArray(array $arr)
+    {
+        return array_chunk($arr, count($arr) / 2);
+    }
+
+    // great this works but we need to cut the array in half, and search the halfs.
+    public function search($arr, $search)
+    {
+        return array_search($search, $arr);
+    }
 }
--- karate-chop/tests/BinaryChop1Test.php
+++ karate-chop/tests/BinaryChop1Test.php
@@ -11,9 +11,33 @@ class BinaryChop1Test extends TestCase
         $this->assertEquals(true, true);
     }
 
+    public function arrays()
+    {
+        return [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        ];
+    }
+
     /** @test */
-    public function chop_class_loads()
+    public function it_chops_an_array_in_half()
     {
         $chop = new BinaryChop1;
+        $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+        $choppedArrays = $chop->halveArray($arr);
+        $expected = count($arr) / 2;
+        $actual = count($choppedArrays[0]);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test*/
+    public function it_returns_the_position_of_3_in_the_array()
+    {
+        $chop = new BinaryChop1;
+        $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+        $search = 3;
+        $actual = $chop->search($arr, $search);
+
+        $this->assertSame(2, $actual);
     }
 }
